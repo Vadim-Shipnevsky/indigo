0266f3b06d7a3d82026b5a9fd0e5bd0221fbd456
vadim <wd.pororo@mail.ru>
2023-04-22 16:17:12 +0300

    feat: add smth

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import styles from '../../styles/Home.module.scss'
+// import styles from '../../styles/Home.module.scss'
+import styles from '../../assets/styles/Home.module.scss'
+
+// import './sss.scss'
 import { useTranslation } from "react-i18next";
 
 const Header = () => {
     const { t } = useTranslation('common');
     return (
+        // <><div className="sss">fdfsdfsdfsdf</div></>
         <div className={styles.header}>
             <div className={styles.logo}>
+                <div className={styles.sss}>sdasdasd</div>
                 {/* <h1>{t("header.personal")}</h1>
                 <h2>{t("header.tarot")}</h2> */}
             </div>
